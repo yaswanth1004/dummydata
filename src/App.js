44cb7033cb665bdfa12d6b3cc80a9571673c1eaf
yaswanth <yaswanth@gmail.com>
2023-10-13 17:30:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import UserList from './components/Record';
+function App() {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Record.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./style.css";
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://dummyjson.com/users')
+      .then(response => {
+        setUsers(response.data.users);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+      console.log(users)
+  }, []);
+
+  return (
+    <div>
+        <h1>Dummy data</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Sno</th>
+            <th>Profile Pic</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Gender</th>
+            <th>E-mail</th>
+            <th>Usename</th>
+            <th>Domain</th>
+            <th>IP</th>
+            <th>University</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td><img src={user.image} alt={user.id} /></td>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+                <td>{user.gender}</td>
+                <td>{user.email}</td>
+                <td>{user.username}</td>
+                <td>{user.domain}</td>
+                <td>{user.ip}</td>
+                <td>{user.university}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserList;
